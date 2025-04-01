0c4e538bb28a5e8a46903b61c0f0ea30b78ae1be
textencoder <160430049+textencoder@users.noreply.github.com>
2025-04-01 18:49:39 -0500

    completed exercises 2.2 & 2.3

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -29,6 +29,7 @@ const Course = (props) => {
     <>
     <Header name={props.course.name} />
     <Content parts={props.course.parts} />
+    <Sum parts={props.course.parts} />
     </>
   )
 }
@@ -42,7 +43,7 @@ const Header = (props) => {
 const Content = (props) => {
     return (
       props.parts.map(part => {   
-        return <Part name={part.name} exercises={part.exercises} />
+        return <Part key={part.name} name={part.name} exercises={part.exercises} />
       })
     )
 }
@@ -50,6 +51,23 @@ const Content = (props) => {
 const Part = (props) => {
   return (
     <p>{props.name} {props.exercises}</p>
+  )
+}
+
+const Sum = (props) => {
+  let totals = []
+  for (const part of props.parts) {
+    for (const [key, value] of Object.entries(part)) {
+      if (key == 'exercises') {
+        totals.push(value)
+      }
+    }
+  }
+  let sum = totals.reduce((acc, el) => acc + el, 0)
+
+
+  return (
+    <strong>total of {sum} exercises</strong>
   )
 }
 
