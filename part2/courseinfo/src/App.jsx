385789447a0d52f3223f1347b6f6b93322ea0556
textencoder <160430049+textencoder@users.noreply.github.com>
2025-04-01 18:29:01 -0500

    completed exercise 2.1

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,56 @@
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+const Course = (props) => {
+  return (
+    <>
+    <Header name={props.course.name} />
+    <Content parts={props.course.parts} />
+    </>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <h1>{props.name}</h1>
+  )
+}
+
+const Content = (props) => {
+    return (
+      props.parts.map(part => {   
+        return <Part name={part.name} exercises={part.exercises} />
+      })
+    )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.name} {props.exercises}</p>
+  )
+}
+
+export default App
